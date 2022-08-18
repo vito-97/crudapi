da218acbef24ff41ffeaf44e0a7015fbe99a0985
维多 <775113283@qq.com>
2022-08-18 14:25:23 +0800

    '优化异常显示'

--- app/exception/ExceptionHandle.php
+++ app/exception/ExceptionHandle.php
@@ -106,7 +106,7 @@ class ExceptionHandle extends Handle
             $msg = '服务器内部错误，请稍后再试';
         }
 
-        if ($request->isAjax() || $request->isOptions() || !env('app_debug')) {
+        if ($request->isAjax() || $request->isOptions() || !env('app_debug') || $e instanceof BaseException) {
             $response = error($code, $msg, $data);
             return $response;
         } else {
